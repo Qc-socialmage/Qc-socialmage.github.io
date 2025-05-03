3e7f880235861beb3761178ad2925534cbb259b1
qcopeland45 <qcopeland45@gmail.com>
2025-05-02 22:07:57 -0600

    quick fix

--- social-mage/postcss.config.js
+++ social-mage/postcss.config.js
@@ -1,11 +1,5 @@
-import tailwindcss, { postcss } from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
-/** @type {import('postcss').AcceptedPlugin[]} */
 export default {
-    plugins: [
-        // tailwindcss/postcss,
-        tailwindcss,
-        autoprefixer
-    ],
-};
+    plugins: {
+      "@tailwindcss/postcss": {},
+    }
+  }
