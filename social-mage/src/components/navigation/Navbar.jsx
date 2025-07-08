906570569bce54862fff6a637a6b602ffc0c9861
qcopeland45 <qcopeland45@gmail.com>
2025-07-07 21:19:50 -0600

    tos and privacy page split

---
+++ social-mage/src/components/navigation/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../assets/styles/Navbar.css';
+
+export default function Navbar() {
+    const [isOpened, setIsOpened] = useState(false);
+    return (
+        <nav className="navbar">
+            <div className="navbar_logo">
+                <Link to="/">SocialMage</Link>
+            </div>
+
+            <button
+                className="navbar_toggle"
+                onClick={() => setIsOpened(!isOpened)}
+            >
+                &#9776; /* hamburger icon */
+            </button>
+
+            <div className={`navbar_menu ${isOpened ? 'active' : ''}`}>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/terms">Terms of Service</Link></li>
+                    <li><Link to="/privacy">Privacy Policy</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
